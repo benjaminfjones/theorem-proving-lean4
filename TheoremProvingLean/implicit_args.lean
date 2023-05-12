dd3917a5e95f0862b0cb9ce586af5101addf8555
Benjamin F Jones <benjaminfjones@gmail.com>
2023-05-11 22:31:43 -0700

    Update notes on implicit args

--- TheoremProvingLean/implicit_args.lean
+++ TheoremProvingLean/implicit_args.lean
@@ -1,5 +1,15 @@
+/-
+Type parameters that can generally be infered from context,
+for example from the types of other explicit parameters, can be
+made implicit using the { } braces.
+-/
+
 universe u
+-- in the `Lst` type def there is no other context to infer from, so α
+-- is explicit
 def Lst (α : Type u) : Type u := List α
+-- in `Lst.cons` and the rest, there is a subsequent parameter whose type
+-- uniquely determines α
 def Lst.cons {α : Type u} (a : α) (as : Lst α) : Lst α := List.cons a as
 def Lst.nil {α : Type u} : Lst α := List.nil
 def Lst.append {α : Type u} (as bs : Lst α) : Lst α := List.append as bs
@@ -21,7 +31,7 @@ def ident {α : Type u} (x : α) := x
 #check ident "hello" -- String
 #check @ident        -- {α : Type u_1} → α → α
 
-/- explicit type annotations to help the ellaborator -/
+/- explicit type annotations to help the elaborator -/
 #check List.nil               -- List ?m
 #check id                     -- ?m → ?m
 
