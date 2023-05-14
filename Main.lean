e9286ef55a99bb2bf775563c4ccb9ad3255cbaa3
Benjamin F Jones <benjaminfjones@gmail.com>
2023-05-13 19:13:43 -0700

    Start universal quantifiers

--- Main.lean
+++ Main.lean
@@ -1,8 +1,9 @@
 import «TheoremProvingLean»
 import TheoremProvingLean.and_commutative
 import TheoremProvingLean.implicit_args
-import TheoremProvingLean.propositions_and_proofs
 import TheoremProvingLean.simple_type_theory
+import TheoremProvingLean.propositions_and_proofs
+import TheoremProvingLean.quantifiers_and_equality
 
 def main : IO Unit :=
   IO.println s!"Hello, {hello}!"
---
+++ TheoremProvingLean/quantifiers_and_equality.lean
@@ -0,0 +1,43 @@
+-- Forall introduction (λ abstraction) and elimination (function application)
+example (α : Type) (p q : α → Prop) : (∀ x : α, p x ∧ q x) → ∀ y : α, p y :=
+  fun h : ∀ x : α, p x ∧ q x =>
+  fun y : α =>
+  show p y from (h y).left
+
+example (α : Type) (p q : α → Prop) : (∀ x : α, p x ∧ q x) → ∀ y : α, q y :=
+  fun h : ∀ x : α, p x ∧ q x =>
+  fun y : α =>
+  show q y from (h y).right
+
+section equiv_relation
+  variable (α : Type) (r : α → α → Prop)
+
+  variable (refl_r : ∀ x, r x x)
+  variable (symm_r : ∀ {x y}, r x y → r y x)
+  variable (trans_r : ∀ {x y z}, r x y → r y z → r x z)
+
+  -- RAD! a ~ b, c ~ b, and c ~ d => a ~ d
+  example (a b c d : α) (hab : r a b) (hcb : r c b) (hcd : r c d) : r a d :=
+    have hbc : r b c := symm_r hcb
+    have hac : r a c := trans_r hab hbc
+    show r a d from trans_r hac hcd
+end equiv_relation
+
+/- Exercises on Universal Quantifiers -/
+section universal_exercises_1
+  variable (α : Type) (p q : α → Prop)
+
+  example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) :=
+    Iff.intro
+      (fun lhs : (∀ x, p x ∧ q x) =>
+        have hp : (∀ x, p x) := fun x : α => (lhs x).left
+        have hq : (∀ x, q x) := fun x : α => (lhs x).right
+        ⟨ hp, hq ⟩)
+      (fun rhs : ((∀ x, p x) ∧ (∀ x, q x)) =>
+        fun x : α =>
+          ⟨rhs.left x, rhs.right x⟩
+      )
+
+  -- example : (∀ x, p x → q x) → (∀ x, p x) → (∀ x, q x) := sorry
+  -- example : (∀ x, p x) ∨ (∀ x, q x) → ∀ x, p x ∨ q x := sorry
+end universal_exercises_1
